b12530050ee55e14be58f98c398b92d224033c2f
Cameron Smith <smithc11@rpi.edu>
2018-01-06 15:45:03 -0500

    opencl: source file for C++ opencl bfs host code

---
+++ partition/Diffusive/src/engpar_opencl_bfs.cpp
@@ -0,0 +1,20 @@
+#include "engpar_queue.h"
+#include "engpar_queue_inputs.h"
+#include "engpar_bfsvisitfn.h"
+#include <PCU.h>
+
+namespace engpar {
+  /*
+    Pull based BFS using OpenCL takes in :
+      graph
+      edge type
+      first seed index into visited array
+      starting depth
+      the visit function
+      inputs object
+  */
+  int bfs_pull_OpenCL(agi::Ngraph* g, agi::etype t,agi::lid_t start_seed,
+               int start_depth, visitFn visit, Inputs* in) {
+  }
+
+}
