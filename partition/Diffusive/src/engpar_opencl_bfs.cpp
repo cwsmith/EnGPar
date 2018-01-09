1d4a3a11d19e431e23cd8ca6dc0b1d0cc6cd5880
Cameron Smith <smithc11@rpi.edu>
2018-01-09 15:11:37 -0500

    ocl: most of pull kernel
    does not work, known write collisions to depth array

--- partition/Diffusive/src/engpar_opencl_bfs.cpp
+++ partition/Diffusive/src/engpar_opencl_bfs.cpp
@@ -11,11 +11,18 @@ cl::CommandQueue* engpar_ocl_queue;
 cl::Device* engpar_ocl_device;
 
 namespace {
+  // copy buffer from the host to the device
   template <typename T>
   cl::Buffer* copyToDevice(T* in, int numVals, int mode) {
     cl::Buffer* d_buff = new cl::Buffer(*engpar_ocl_context, mode, numVals*sizeof(T));
     cl::copy(*engpar_ocl_queue, in, in+numVals, *d_buff);
     return d_buff;
+  }
+
+  // copy buffer from the device to the host
+  template <typename T>
+  void copyFromDevice(cl::Buffer* d_buff, T* h_buff, int numVals) {
+    cl::copy(*engpar_ocl_queue, *d_buff, h_buff, h_buff+numVals);
   }
 
   cl::Program* createProgram(std::string kernelFileName) {
@@ -39,7 +46,7 @@ namespace engpar {
     Pull based BFS using OpenCL takes in :
       graph
       edge type
-      first seed index into visited array
+      first seed index into depth array
       starting depth
       the visit function
       inputs object
@@ -51,15 +58,31 @@ namespace engpar {
 
     std::cout << "OpenCL calling make_kernel." << std::endl << std::endl;
     cl::make_kernel
-      <cl::Buffer, cl::Buffer, cl::Buffer, cl::Buffer, cl_long, cl_long, cl_long>
+      <cl::Buffer,        //degreeList
+       cl::Buffer,        //edgeList
+       cl::Buffer,        //pinDegreeList
+       cl::Buffer,        //pinList
+       cl_long,           //numVerts
+       cl_long,           //numEdges
+       cl_long,           //numPins
+       cl::Buffer,        //depth
+       cl::Buffer,        //seeds
+       cl_long,           //numSeeds
+       cl_int,            //startDepth
+       cl::LocalSpaceArg> //localWork
       bfsPullKernel(*program, "bfskernel");
+
+    // initialize the visited/depth array
+    agi::PNgraph* pg = g->publicize();
+    for (agi::lid_t i=start_seed;i<in->numSeeds;i++) 
+      in->visited[in->seeds[i]] = start_depth;
 
     ////////
     // copy the graph CSRs to the device
     ////////
-    agi::PNgraph* pg = g->publicize();
-    printf("host: numVerts numEdges numPins %ld %ld %ld\n",
-           pg->num_local_verts, pg->num_local_edges[t], pg->num_local_pins[t]);
+    printf("host: numVerts numEdges numPins numSeeds startDepth %ld %ld %ld %ld %d\n",
+           pg->num_local_verts, pg->num_local_edges[t], pg->num_local_pins[t],
+           in->numSeeds, start_depth);
     // vert-to-nets
     cl::Buffer* d_degreeList = copyToDevice<agi::lid_t>(
         pg->degree_list[t],
@@ -78,19 +101,42 @@ namespace engpar {
         pg->pin_list[t],
         pg->num_local_pins[t],
         CL_MEM_READ_ONLY);
+    ////////
+    // vertex depth array
+    ////////
+    cl::Buffer* d_depth = copyToDevice<int>(
+        in->visited,
+        pg->num_local_edges[t],
+        CL_MEM_READ_WRITE);
+    cl::Buffer* d_seeds = copyToDevice<agi::lid_t>(
+        in->seeds,
+        pg->num_local_edges[t],
+        CL_MEM_READ_ONLY);
+    cl::LocalSpaceArg localWork = cl::Local(sizeof(int)*sizeof(pg->num_local_verts));
 
     std::cout << "OpenCL initialization complete." << std::endl << std::endl;
-    cl::NDRange global(pg->num_local_edges[t]);
+    cl::NDRange global(pg->num_local_verts);
 
     std::cout << "OpenCL kernel queued" << std::endl << std::endl;
     bfsPullKernel(cl::EnqueueArgs(*engpar_ocl_queue, global),
         *d_degreeList, *d_edgeList, *d_pinDegreeList, *d_pinList,
-        pg->num_local_verts, pg->num_local_edges[t], pg->num_local_pins[t]);
+        pg->num_local_verts, pg->num_local_edges[t], pg->num_local_pins[t],
+        *d_depth, *d_seeds, in->numSeeds, start_depth, localWork);
+
+    copyFromDevice<int>(d_depth, in->visited, pg->num_local_edges[t]);
+
+    // TODO: reconstruct seeds array from depth array
+    //  the serial implementation computes the seeds array on the fly, but that
+    //  is hard to do in a parallel implementation without using atomics
+    // The seeds array contains contiguous groups of edges with the same depth
+    // in ascending order (first group is depth 1).
 
     delete d_degreeList;
     delete d_edgeList;
     delete d_pinDegreeList;
     delete d_pinList;
+    delete d_depth;
+    delete d_seeds;
     delete program;
   }
 
