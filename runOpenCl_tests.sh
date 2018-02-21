db41752230ca5fc5e4152fccc8bd17af27faa29f
Cameron Smith <smithc11@rpi.edu>
2018-02-21 15:44:06 -0500

    ocl: test script supports chunk size sweep and scg

--- runOpenCl_tests.sh
+++ runOpenCl_tests.sh
@@ -12,14 +12,14 @@ srcDir=$3
 [ ! -e $bin ] && "$bin does not exist" && exit 1
 [ ! -e $srcDir ] && "$srcDir does not exist" && exit 1
 
-cp $srcDir/partition/Diffusive/src/bfskernel.cl .
+cp $srcDir/partition/Diffusive/src/*.cl .
 
+#aero1Belm/graph128Ki_88637
+#aero1Belm/graph128Ki_25114
+#aero1Belm/afterMigr_81334
+#aero1Belm/afterMigr_1457
+#cube/cube
 read -r -d '' tests <<'EOF'
-aero1Belm/graph128Ki_88637
-aero1Belm/graph128Ki_25114
-aero1Belm/afterMigr_81334
-aero1Belm/afterMigr_1457
-cube/cube
 upright/67k
 upright/190k
 upright/400k
@@ -27,37 +27,51 @@ upright/890k
 upright/1.6M
 EOF
 
-bfsModeString=("push" "pull" "opencl")
-for bfsmode in 0 1 2; do
+bfsModeString=("push" "pull" "csropencl" "scgopencl")
+
+for bfsmode in 0 1 2 3; do
   echo "${bfsModeString[$bfsmode]} bfs"
+  kernel=""
+  [ $bfsmode == 2 ] && kernel="--kernel bfsCsrKernel.cl"
+  [ $bfsmode == 3 ] && kernel="--kernel bfsScgKernel.cl"
+  chunkRange=(1)
+  [ $bfsmode == 3 ] && chunkRange=(16 32 64 128 256 512 1024 2048)
   for t in ${tests[@]}; do
     graphName=${t##*/}
-    for i in {1..5}; do
-      $bin --device 0 --graph ${graphDir}/$t --bfsmode $bfsmode >> ${bfsModeString[$bfsmode]}_${graphName}.log
+    for c in ${chunkRange[@]}; do
+      echo graph $t bfsmode $bfsmode $kernel chunk $c
+      for i in {1..3}; do
+        $bin --device 0 --graph ${graphDir}/$t --bfsmode $bfsmode $kernel --chunkSize $c >> ${bfsModeString[$bfsmode]}_chunk${c}_${graphName}.log
+      done
     done
   done
 done
 
 cmdString=('/push bfs time/ {sum+=$6; cnt+=1} END {print sum/cnt}' 
            '/pull bfs time/ {sum+=$6; cnt+=1} END {print sum/cnt}'
+           '/^opencl bfs time/ {sum+=$5; cnt+=1} END {print sum/cnt}'
            '/^opencl bfs time/ {sum+=$5; cnt+=1} END {print sum/cnt}')
-for bfsmode in 0 1 2; do
+for bfsmode in 0 1 2 3; do
   outlog=${bfsModeString[$bfsmode]}AvgTimes.csv
   cat /dev/null > $outlog
   echo "${bfsModeString[$bfsmode]} bfs"
+  chunkRange=(1)
+  [ $bfsmode == 3 ] && chunkRange=(16 32 64 128 256 512 1024 2048)
   for t in ${tests[@]}; do
     graphName=${t##*/}
-    inlog=${bfsModeString[$bfsmode]}_${graphName}.log
-    avg=$(awk "${cmdString[$bfsmode]}" $inlog)
-    echo "$graphName,$avg" >> $outlog
+    for c in ${chunkRange[@]}; do
+      inlog=${bfsModeString[$bfsmode]}_chunk${c}_${graphName}.log
+      avg=$(awk "${cmdString[$bfsmode]}" $inlog)
+      echo "$graphName,$c,$avg" >> $outlog
+    done
   done
 done
 
-#print number of vertices and edges in each test graph
-outlog=graphs.csv
-cat /dev/null > $outlog
-for t in ${tests[@]}; do
-  graphName=${t##*/}
-  vertsAndEdges=$(awk '/numVtx/ {print $7","$8; exit}' opencl_${graphName}.log)
-  echo "$graphName,$vertsAndEdges" >> $outlog
-done
+##print number of vertices and edges in each test graph
+#outlog=graphs.csv
+#cat /dev/null > $outlog
+#for t in ${tests[@]}; do
+#  graphName=${t##*/}
+#  vertsAndEdges=$(awk '/numVtx/ {print $7","$8; exit}' opencl_${graphName}.log)
+#  echo "$graphName,$vertsAndEdges" >> $outlog
+#done
